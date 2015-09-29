005da5ac580c502a535ac9f629ffc0b2c3fb1612
Safiyat Reza <md.reza@snapdeal.com>
2015-09-30 00:16:55 +0530

    Corrected install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo chown -R safiyat /opt/logstash-1.5.4
 sudo tar xvf elasticsearch-1.7.2.tar.gz -C /opt/
 sudo chown -R safiyat /opt/elasticsearch-1.7.2
 unzip -d /opt/elasticsearch-1.7.2/plugins/ elasticsearch-head-master.zip
-mv /opt/elasticsearch-1.7.2/plugins/elasticsearch-head-master head
+mv /opt/elasticsearch-1.7.2/plugins/elasticsearch-head-master /opt/elasticsearch-1.7.2/plugins/head
 
 sudo tar xvf kibana-4.1.2-linux-x64.tar.gz -C /opt/
 sudo chown -R safiyat /opt/kibana-4.1.2-linux-x64
